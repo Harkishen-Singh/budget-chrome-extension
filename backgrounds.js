9c8c871c5c71ef0ea80bdb5d6d141e98e93c36a7
Harkishen-Singh <harkishensingh@hotmail.com>
2018-09-04 21:07:40 +0530

    added support for content script

--- backgrounds.js
+++ backgrounds.js
@@ -4,6 +4,14 @@ chrome.contextMenus.create({
     "contexts":["selection"]
 });
 
+
 chrome.storage.onChanged.addListener((changes, storageName)=>{
     chrome.browserAction.setBadgeText({"text":changes.totalCredits.newValue.toString()});
+})
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request.hey == "thisisaHeyMessage"){
+        console.warn('received HEY MESSAGE')
+        alert('received a hey message')
+    }
 })
