0da24c17d06c1e6352c80bdfd8a1813383323c08
Harkishen-Singh <harkishensingh@hotmail.com>
2018-09-04 19:47:02 +0530

    basic budget management extension

---
+++ controller.js
@@ -0,0 +1,59 @@
+$(document).ready(() => {
+
+    chrome.storage.sync.get('totalDebits', (credits) => {
+        let amt = parseInt(credits.totalDebits);
+        document.getElementById('bal_a').innerHTML = amt;
+        // $("#bal_a").val() = netAmount;
+        console.warn('thisss')
+        console.warn(isNaN(amt))
+    })
+    chrome.storage.sync.get('totalCredits', (credits) => {
+        let amt = parseInt(credits.totalCredits);
+        if(credits.totalCredits==undefined)
+        document.getElementById('cre_a').innerHTML = 0;
+        else
+        document.getElementById('cre_a').innerHTML = amt;
+        // $("#bal_a").val() = netAmount;
+    })
+    
+    $("#addAmt").click(() => {
+        var netAmount=0
+        console.warn('adding '+ $("#amount").val())
+        netAmount =  netAmount+ parseInt($("#amount").val()) 
+        let curr_debit = parseInt($("#amount").val()) 
+        chrome.storage.sync.get('totalCredits', credits => {
+            let credit =parseInt(credits.totalCredits) ;
+            if (credit >= curr_debit) {
+                credit -= curr_debit;
+                chrome.notifications.create('cutNotify', {
+                    type:'basic',
+                    title:'Debited from Balance',
+                    iconUrl:'icon.jpg',
+                    message: curr_debit+' have been debited from your account.'
+                })
+                chrome.storage.sync.set({'totalCredits':credit})
+            }
+            else
+                chrome.notifications.create('noBalanceWarning', {
+                    type:'basic',
+                    title:'No Balance',
+                    iconUrl:'icon.jpg',
+                    message:'Your Account has Zero balance. Please add credits to continue further shopping.'
+                })
+            
+        })
+
+        chrome.storage.sync.get('totalDebits', (credits) => {
+            let amt = parseInt(credits.totalDebits);
+            if(!isNaN(amt))
+            netAmount += amt;
+
+
+            chrome.storage.sync.set({'totalDebits': parseInt(netAmount) });
+            document.getElementById('bal_a').innerHTML = netAmount;
+
+        })
+        
+    })
+
+})
---
+++ creditController.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+    chrome.storage.sync.get('totalCredits', credits => {
+        if(credits.totalCredits==undefined)
+        document.getElementById('showCredits').innerHTML = 0;
+        else
+        document.getElementById('showCredits').innerHTML = credits.totalCredits;
+    })
+    $("#add").click(() => {
+        chrome.storage.sync.get('totalCredits', credits => {
+            let previsCredit = parseInt(credits.totalCredits) ;
+            let nowCredit = parseInt($("#addCredit").val()) ;
+            if(credits.totalCredits!=undefined)
+            nowCredit+= previsCredit;
+            else
+            nowCredit = previsCredit = 0
+            chrome.storage.sync.set({'totalCredits':nowCredit})
+            document.getElementById('showCredits').innerHTML = nowCredit;
+            close();
+        })
+    })
+})
